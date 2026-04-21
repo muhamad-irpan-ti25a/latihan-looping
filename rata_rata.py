60e392e8abff33c8d0ff0b695a5cdefe19f72a4c
Muhamad Irpan <muhamad.irpan_ti25@nusaputra.ac.id>
2026-04-21 10:03:02 +0700

    Fix casing in input prompts and output message

--- rata_rata.py
+++ rata_rata.py
@@ -1,9 +1,9 @@
 total = 0
 
 for i in range(1, 6):
-    nilai = int(input(f"Masukkan nilai ke-{i}: "))
+    nilai = int(input(f"masukkan nilai ke-{i}: "))
     total += nilai
 
 rata_rata = total / 5
 
-print("Rata-rata:", rata_rata)
+print("rata-rata:", rata_rata)
